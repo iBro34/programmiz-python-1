9f4d90137f0c53573194e15bccfc2abcfad9ed08
Ikhtiyor <ikhtiyor9207@gmail.com>
2020-11-05 22:19:48 +0500

    check

---
+++ if.py
@@ -0,0 +1,9 @@
+a = int(input("Please enter a number"))
+if a > 0:
+    print(a, "it is positive")
+elif a < 0:
+    print(a,"it is negative")
+elif a == 0:
+    print(a,"it is zero")
+else:
+    print("error")

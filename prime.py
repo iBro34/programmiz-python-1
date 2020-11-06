3dbef48a8f68a58c8c0470b0d2c2d98cd0f4dcf5
Ikhtiyor <ikhtiyor9207@gmail.com>
2020-11-06 20:31:36 +0500

    prime

--- prime.py
+++ prime.py
@@ -1,11 +1,12 @@
 num = int(input("Please enter a number"))
-if num > 1:
-    for i in range(2,num):
-        if(num%2)==0:
-            print(num,"is not prime number")
-            print(i,"times",num//i ,"is",num)
-            break
-        else:
-            print(num,"it is a prime number")
-else:
-   print(num,"is not a prime number")
+    if num > 1:
+        for i in range(2,num):
+            if(num%2)==0:
+                print(num ,"is not a prime a number")
+                print(i,"times",num//i,"is",num)
+                break
+            else:
+                print(num,"is prime number")
+    else:
+        print("Error")
+    

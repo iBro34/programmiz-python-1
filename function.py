6d1fdc9e42ee70db6693ca50612f7ff9639ef782
Ikhtiyor <ikhtiyor9207@gmail.com>
2020-11-10 22:51:21 +0500

    function

---
+++ function.py
@@ -0,0 +1,6 @@
+def colour(name,age,color):
+    print("Hi your name is",name," you are",age," years old")
+    print("Your favorite color is ",color)
+name = str(input("Please enter your name"))
+age = int(input("Please enter your age"))
+colour(name,age,"blue")

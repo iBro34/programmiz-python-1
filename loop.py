a55b9e08119cd2420f218c9cf0093b0f618f6f9d
Ikhtiyor <ikhtiyor9207@gmail.com>
2020-11-17 22:59:50 +0500

    loop1

---
+++ loop.py
@@ -0,0 +1,7 @@
+friends = ['John','Terrry','Eric','Michael','George']
+for  friend in friends:
+    if friend == 'Eric':
+        print("Here !" + friend)
+        break
+    print(friend)
+print("For Loop Done")
